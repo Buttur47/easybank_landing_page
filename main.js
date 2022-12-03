73fa01c58cb99725b23d550fa7770ae73ace7fc7
unknown <kcssowerby@gmail.com>
2022-12-03 15:46:12 -0500

    mobile version up. Need responsive display

--- main.js
+++ main.js
@@ -4,10 +4,14 @@ document.addEventListener("DOMContentLoaded", function(event){
 
   drp_butt.onclick = function() {
     var drp = document.getElementById("dropdown_menu");
-    var shadow = document.getElementById("light_room")
+    var shadow = document.getElementById("light_room");
+    var menu = document.getElementById("menu");
+    var exit = document.getElementById("exit_menu");
     drp.classList.toggle('hidden');
     shadow.classList.toggle('hidden');
-    document.html.style.overflow = "hidden";
+    menu.classList.toggle('hidden');
+    exit.classList.toggle('shown');
+    document.body.classList.toggle("no_scroll");
   };
 
 })
